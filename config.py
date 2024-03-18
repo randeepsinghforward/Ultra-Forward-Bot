fe2035764de25f6978ada6238360c9964ccce63d
randeepsinghforward <163434596+randeepsinghforward@users.noreply.github.com>
2024-03-18 11:37:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ import os
 class Config:
     API_ID = os.environ.get("API_ID", "24253579")
     API_HASH = os.environ.get("API_HASH", "55773656b8e938d38ea528f26a29a2b3")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6777602514:AAG9aL4nsjgFSSQhPYVgrmVAmXVtSl_EQfs") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://forward48:aaaa11@forward48.yvqr9fl.mongodb.net/?retryWrites=true&w=majority&appName=forward48")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://forward48:aaaa11@forward48.afpughx.mongodb.net/?retryWrites=true&w=majority&appName=forward48")
     DB_NAME = os.environ.get("DB_NAME", "forward48")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '5311861748').split()]
 
